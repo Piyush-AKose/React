cd4e0459625d6f21d975d280351daa08ffdf1594
Piyushkose8956 <piyushkose.04@gmail.com>
2025-06-13 22:46:35 +0530

    PasswordGenerator

---
+++ 05passwordGenerator/src/App.jsx
@@ -0,0 +1,35 @@
+import { useCallback } from "react";
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [number, setNumber] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (number) str += " 0123456789";
+    if (charAllowed) str += "!@#$%^&*()";
+
+    for (let i = 1; i <= array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass = str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, number, charAllowed, setPassword]);
+
+  return (
+    <>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-800">
+        {" "}
+        test{" "}
+      </div>
+    </>
+  );
+}
+
+export default App;
